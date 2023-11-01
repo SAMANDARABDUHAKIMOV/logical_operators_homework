fc51073dda37fdce7730927ea234dc56c956a1c7
samandar <samandar.abduhakimov.007@gmail.com>
2023-11-01 10:42:51 +0500

    fd

--- logic20.py
+++ logic20.py
@@ -8,5 +8,23 @@ def main(n):
     Returns:
         bool: answer
     """
+    x1=n%10
+    n//10
 
-    return
+    x2=n%10
+    n//10
+
+    x3=n%10
+    n//=10
+    
+    x4=n%10
+    n//=10
+    
+    x5=n%10
+    n//10
+    ones=x1+x2+x3+x4+x5
+    zeros=5-ones
+
+
+    return  ones>zeros
+print(main(10101))
