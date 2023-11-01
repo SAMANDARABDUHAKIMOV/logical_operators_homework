323d7c31ac597048ab1aabecbb53ed43fd62c3fc
samandar <samandar.abduhakimov.007@gmail.com>
2023-11-01 10:00:30 +0500

    cc

--- logic12.py
+++ logic12.py
@@ -4,6 +4,8 @@ def main(a):
     Args:
         a(int): parameter a
     Returns:
-        bool: answer
+        bool:  
     """
-    return
+    return  a>9 and a<100 and a%10==a//10
+print(main(32))
+print(main(22)) 
